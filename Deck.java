461d808c370b43f8da700ceb420ee08cc01bb05e
FlingusMcDingus <zelekbarrett@gmail.com>
2021-06-30 19:58:49 -0400

    Edit and Complete Deck Object

--- Deck.java
+++ Deck.java
@@ -1,31 +1,35 @@
 import java.util.ArrayList;
+import java.util.*;
 
 public class Deck {
 
     Card newCard;
-    ArrayList<Card> newDeck;
+    ArrayList<Card> newDeck = new ArrayList<Card>();
 
-    public Deck() {
+    public Deck(ArrayList<Card> fillDeck) {
         for (int i=0; i<4; i++) {
-            for (int j=0; j<13; j++) {
-                newCard = new Card(0, j);
-                newDeck.add(newCard);
-            }
-
-            for (int j=0; j<13; j++) {
-                newCard = new Card(1, j);
-                newDeck.add(newCard);
-            }
-
-            for (int j=0; j<13; j++) {
-                newCard = new Card(2, j);
-                newDeck.add(newCard);
-            }
-
-            for (int j=0; j<13; j++) {
-                newCard = new Card(3, j);
+            for (int j=1; j<14; j++) {
+                newCard = new Card(i, j);
                 newDeck.add(newCard);
             }
         }
+    }
+
+    public Card drawCard() {
+        if (newDeck.size() != 0) {
+            Card drawnCard = newDeck.get(0);
+            newDeck.remove(0);
+            return drawnCard;
+        }
+        return null;
+    }
+
+    public void replaceCard(Card replacementCard) {
+        newDeck.add(replacementCard);
+    }
+
+    public void shuffle() {
+        Random rand = new Random(52);
+        Collections.shuffle(newDeck, rand);
     }
 }
