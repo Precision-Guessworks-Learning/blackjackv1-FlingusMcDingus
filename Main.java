7621cc996a0e82de734df069f965fc7dcbf2c5c8
FlingusMcDingus <zelekbarrett@gmail.com>
2021-07-14 20:36:32 -0400

    Edit and Continue Creating Game

--- Main.java
+++ Main.java
@@ -8,6 +8,7 @@ public class Main{
         newDeck.shuffle();
         Hand newHand = new Hand();
         Boolean gameOn = true;
+        Scanner hitOrStay = new Scanner(System.in);
         
         System.out.println("Game Start.");
         System.out.println("You draw two cards to start");
@@ -15,16 +16,27 @@ public class Main{
         newCard = newDeck.drawCard();
         System.out.println(newCard.getName(newCard.getVal(), newCard.getSuit()));
         int firstVal = checkForBigNum(newCard);
-
+        //firstVal = checkForAce(firstVal);
+        
         newCard = newDeck.drawCard();
         System.out.println(newCard.getName(newCard.getVal(), newCard.getSuit()));
         int newVal = checkForBigNum(newCard);
 
         System.out.println("Your total is " + (firstVal + newVal));
         firstVal = firstVal + newVal;
+        System.out.println(firstVal);
 
         while (gameOn == true) {
-
+            System.out.println("Hit?\n\n1)Hit me\n2)Stay");
+            int hit = hitOrStay.nextInt();
+            switch (hit) {
+                case 1:
+                newCard = newDeck.drawCard();
+                System.out.println(newCard.getName(newCard.getVal(), newCard.getSuit()));
+                int newValTwo = checkForBigNum(newCard);
+                System.out.println("Your total is " + (firstVal + newValTwo));
+                firstVal = firstVal + newValTwo;
+            }
         }
         
     }
@@ -37,4 +49,16 @@ public class Main{
             return cardVal;
         }
     }
+
+    /*public static int checkForAce(int checkCard) {
+        Scanner k = new Scanner(System.in);
+        if (checkCard == 1) {
+            System.out.println("You got an Ace. Is it worth one, or ten?");
+            int val = k.nextInt();
+            while (val != 1 || val != 10) {
+                System.out.println("");
+            }
+            return val;
+        }
+    }*/
 }
